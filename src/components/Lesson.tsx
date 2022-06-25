3d682e28d132f3cb769c98f857df5d7b4aa59475
Emerson Trindade <trinddade29@gmail.com>
2022-06-25 20:43:19 -0300

    add bg link

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,7 +1,8 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast, format } from 'date-fns';
 import ptBR from "date-fns/locale/pt-BR";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import classNames from "classnames"
 
 interface lessonProps {
   title: string;
@@ -11,6 +12,7 @@ interface lessonProps {
 }
 
 export function Lesson(props: lessonProps) {
+  const {slug} = useParams();
   const isLessonAvailable = isPast(props.availableAt);
   const availableDateFormatted = format(
     props.availableAt,
@@ -20,19 +22,22 @@ export function Lesson(props: lessonProps) {
     }
   )
 
+  const isActiveLesson = slug === props.slug
+
   return (
     <Link to={`/event/lesson/${props.slug}`} className="group">
-      <span className="text-gray-300">
+      {/* <span className="text-gray-300">
         {availableDateFormatted}
-      </span>
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+      </span> */}
+      <div className={classNames("rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500",{
+        'bg-green-500': isActiveLesson
+      })}>
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
               <CheckCircle size={20} />
               Conteúdo liberado
             </span>
-
           ) : (
             <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
               <Lock size={20} />
