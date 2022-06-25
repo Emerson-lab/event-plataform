c2241024c55aeb96add698eab54751a474a3fcfd
Emerson Trindade <trinddade29@gmail.com>
2022-06-25 20:14:27 -0300

    rename variable

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client"
 export const client = new ApolloClient({
  uri: import.meta.env.VITE_API_URL,
  headers: {
-  'Athorazation': `Bearer${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  'Athorazation': `Bearer${import.meta.env.VITE_API_AUTORIZATION_TOKEN}`
  },
  cache: new InMemoryCache()
 })
